5db35e04f82f34ffeea2022d47c66bf606a9efa1
Leonardo Silva <leodasilva.2012@hotmail.com>
2022-11-16 00:11:02 -0300

    make the player factory function and the gameboard object

---
+++ script.js
@@ -0,0 +1,28 @@
+// player factory function
+const createPlayer = (name, marker) => {
+    return {name, marker};
+}
+
+// gameboard object
+const gameBoard = (() => {
+
+    // generate board array
+    let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push('');
+    }
+
+    // display square for each array item
+    let squares = document.querySelector('.squares');
+
+    board.forEach((item, index) => {
+        const square = document.createElement('div');
+        square.className = 'square';
+        squares.appendChild(square);
+    })
+
+    // return
+    return {
+        board
+    };
+})();
